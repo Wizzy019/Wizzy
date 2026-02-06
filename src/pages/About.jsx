2ca432af9898f8ef92e71c17ee136840beb2038e
Wizzy019 <kingwizzyndukwu@gmail.com>
2026-02-06 16:28:45 +0000

    omor

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -40,7 +40,7 @@ function About() {
               I aim to be a developer who can be trusted to take ownership, ship responsibly, and deliver practical, well-thought-out solutions.
               </p>
               <a
-                href="public/Wisdom_Izuchukwu_Ndukwu_Resume.pdf"
+                href="/Wisdom_Izuchukwu_Ndukwu_Resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-max bg-blue-900 px-6 py-3 rounded hover:bg-blue-800 transition font-semibold"
